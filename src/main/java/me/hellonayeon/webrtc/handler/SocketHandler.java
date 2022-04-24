8eb4771ec00fe6a0999cf53540814f9e85be959c
hellonayeon <hellonykwon@gmail.com>
2022-04-24 21:55:35 +0900

    create: 웹 소켓 핸들러 생성

---
+++ src/main/java/me/hellonayeon/webrtc/handler/SocketHandler.java
@@ -0,0 +1,29 @@
+package me.hellonayeon.webrtc.handler;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class SocketHandler extends TextWebSocketHandler {
+
+    List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException { // (+) InterruptedException ?
+        // FIXME: login, offer, answer, candidate 다르게 처리 필요
+        for (WebSocketSession webSocketSession : sessions) {
+            if (webSocketSession.isOpen() && !session.getId().equals(webSocketSession.getId())) {
+                webSocketSession.sendMessage(message);
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+}
