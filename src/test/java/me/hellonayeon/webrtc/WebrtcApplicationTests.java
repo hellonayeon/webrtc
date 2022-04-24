75fb148c1dee3342a96c81757b6793836dd8f3a6
hellonayeon <hellonykwon@gmail.com>
2022-04-24 18:03:11 +0900

    create: 프로젝트 생성

---
+++ src/test/java/me/hellonayeon/webrtc/WebrtcApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hellonayeon.webrtc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebrtcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
